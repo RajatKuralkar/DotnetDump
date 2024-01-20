3cd0abac74396216f2c6c43b4cee5c32c24c00b1
RajatKuralkar <rajatkuralkar2@gmail.com>
2024-01-20 19:15:20 +0530

    added a new shop in AddDbContext

--- MVC/practice/Data/AppDbContext.cs
+++ MVC/practice/Data/AppDbContext.cs
@@ -14,11 +14,13 @@ public class AppDbContext:DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // module builder
         modelBuilder.Entity<Shop>().HasData(
             new Shop(){Pid=101, Pname="Rajat Shop-1", Pprice=29999.99},
             new Shop(){Pid=102, Pname="Rajat Shop-2", Pprice=29999.99},
             new Shop(){Pid=103, Pname="Rajat Shop-3", Pprice=29999.99},
-            new Shop(){Pid=104, Pname="Rajat Shop-4", Pprice=29999.99}
+            new Shop(){Pid=104, Pname="Rajat Shop-4", Pprice=29999.99},
+            new Shop(){Pid=105, Pname="Rajat Shop-5", Pprice=29999.99}
             
         );
     }
