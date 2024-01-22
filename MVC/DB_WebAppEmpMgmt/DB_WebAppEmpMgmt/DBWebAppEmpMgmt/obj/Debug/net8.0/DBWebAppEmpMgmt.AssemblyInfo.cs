f88e5514f25a2a98e64d0199f0257f9b49d325d5
RajatKuralkar <rajatkuralkar2@gmail.com>
2024-01-22 18:53:08 +0530

    // new Commit

--- MVC/DB_WebAppEmpMgmt/DB_WebAppEmpMgmt/DBWebAppEmpMgmt/obj/Debug/net8.0/DBWebAppEmpMgmt.AssemblyInfo.cs
+++ MVC/DB_WebAppEmpMgmt/DB_WebAppEmpMgmt/DBWebAppEmpMgmt/obj/Debug/net8.0/DBWebAppEmpMgmt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBWebAppEmpMgmt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd0abac74396216f2c6c43b4cee5c32c24c00b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBWebAppEmpMgmt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBWebAppEmpMgmt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
